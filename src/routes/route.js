e6f7fc0d8f7ade2f76818a6f41e6e9d13e5ea214
Vipulvj1 <98739460+Vipulvj1@users.noreply.github.com>
2022-02-26 17:25:46 +0530

    Update route.js

--- src/routes/route.js
+++ src/routes/route.js
@@ -77,20 +77,23 @@ let persons= [
     votingStatus: false
 }
 ]
-let eligiblePerson = []
-router.post('/post_query_ageCheck',function(req, res){
-let age = req.query.votingAge
-console.log(age)
-for (let i=0; i<persons.length; i++){
-if(age < persons[i].age){
-    persons[i].votingStatus = true
-    eligiblePerson.push(persons[i].name)
-}
-}
-console.log(persons)
-console.log(eligiblePerson)
-return res.send({personsArray :persons,eligiblePersonArray :eligiblePerson})
-})
+router.post("/post-query-2", function (req, res) {
+    
+    let votingAge = req.query.age;
+    let validVoters = [];
+
+     for (let i = 0; i < persons.length; i++) {
+
+       if (persons[i]["age"] > votingAge) {
+          persons[i]["votingStatus"] = true;
+           validVoters.push(persons[i]);
+         }
+
+     }
+
+     res.send(validVoters);
+});
+
 
 
 
